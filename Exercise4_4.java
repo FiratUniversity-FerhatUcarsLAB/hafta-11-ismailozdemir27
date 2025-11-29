84f3f9f6998d8420c47d42e1988a7532ba12dbf1
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-11-29 19:20:37 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -19,3 +19,33 @@ public class Exercise4_4 {
         // Cevaplarinizi yorum olarak ekleyin.
     }
 }
+1-Derleyici HATA vermez.
+	Derleyici UYARI da vermez IDE uyarı gösterebilir ama Java derleyicisi göstermez.
+	Program sorunsuz çalışır.
+
+	Açıklama:
+	Java’da return eden bir metodun sonucunu kullanmamak tamamen yasaldır.
+	Sadece IDE şöyle bir uyarı verebilir:
+
+	“Result of method call is ignored”
+
+	Ama bu Java’nın verdiği bir uyarı değildir — sadece IDE yardım amaçlı söyler.
+
+	Sonuç:
+	Hiçbir şey olmaz, program normal çalışır.
+
+	2-sayHello() bir void metod - değer döndürmez.
+
+	Ancak println(sayHello() + 7) bir ifade (expression) oluşturuyor:
+
+	sayHello() bir değermiş gibi işlem yapmaya çalışıyor.
+
+	void + 7 → anlamsız
+
+	Java şöyle diyor:
+
+	“Ben değer döndürmeyen (void) bir şeyi matematiksel işlemde kullanamam.”
+
+	Sonuç:
+	Kod derlenmez.
+	Hata: “void cannot be converted to int”.
