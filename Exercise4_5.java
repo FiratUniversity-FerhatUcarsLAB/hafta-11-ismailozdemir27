097243ad2a3ac2495d125229a18fb9daf9486415
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-11-29 19:21:34 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -25,3 +25,42 @@ public class Exercise4_5 {
         System.out.println("any " + strangStrung + "more ");
     }
 }
+
+public class Exercise4_5 {
+
+    public static void zoop(String fred, int bob) {
+        System.out.println(fred);
+        if (bob == 5) {
+            ping("not ");
+        } else {
+            System.out.println("!");
+        }
+    }
+
+    public static void main(String[] args) {
+        int bizz = 5;
+        int buzz = 2;
+        zoop("just for", bizz);
+        clink(2 * buzz);
+    }
+
+    public static void clink(int fork) {
+        System.out.print("It's ");
+        zoop("breakfast ", fork);
+    }
+
+    public static void ping(String strangStrung) {
+        System.out.println("any " + strangStrung + "more ");
+    }
+}
+
+STACK DİYAGRAMI 
+    main → clink(4) → zoop(" breakfast ", 4) 
+Zoop ikinci kez clink(4) içinden çağrılır.
+
+TAM ÇIKTI
+     just for 
+ any  not  more 
+It ’s 
+ breakfast 
+!
