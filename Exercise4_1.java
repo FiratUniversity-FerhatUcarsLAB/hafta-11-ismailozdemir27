933ed9fd79647916bc69d88e44c15c03ffb058d9
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-11-29 18:47:57 +0300

    Add date formatting methods and user input handling
    Implemented methods to print dates in American and European formats, and added user input functionality.

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,40 @@
-public class Exercise4_1 {
+package odev1;
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
-    }
+import java.util.Scanner;
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
-    }
+public class dateformation {
 
-    public static void main(String[] args) {
-        // TODO: test methods
-    }
-}
+    // American format: Monday, July 22, 2019
+       public static void printAmerican(String day, int date, String month, int year) {
+           System.out.println(day + ", " + month + " " + date + ", " + year);
+       }
+
+       // European format: 22 July 2019, Monday
+       public static void printEuropean(String day, int date, String month, int year) {
+           System.out.println(date + " " + month + " " + year + ", " + day);
+       }
+
+       public static void main(String[] args) {
+
+           Scanner input = new Scanner(System.in);
+
+           // --- Kullanıcıdan bilgi alma bölümü ---
+           System.out.print("Enter day (örn, Monday): ");
+           String day = input.nextLine();
+
+           System.out.print("Enter month (örn , July): ");
+           String month = input.nextLine();
+
+           System.out.print("Enter date (number): ");
+           int date = input.nextInt();
+
+           System.out.print("Enter year: ");
+           int year = input.nextInt();
+
+           System.out.println("\nAmerican Format:");
+           printAmerican(day, date, month, year);
+
+           System.out.println("European Format:");
+           printEuropean(day, date, month, year);
+       }
+   }
