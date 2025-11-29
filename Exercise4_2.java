f4a5b953b7c4760d862839cd8ab76dbc4b37fea6
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-11-29 19:12:37 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,23 @@
+package odev2;
+
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+	public static void main(String[] args) {
+	    zippo("rattle", 13);        // 1
+	}
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+	public static void baffle(String blimp) {
+	    System.out.println(blimp);  // 5
+	    zippo("ping", -5);          // 6
+	}
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
-
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
-        }
-    }
+	public static void zippo(String quince, int flag) {
+	    if (flag < 0) {             // 7
+	        System.out.println(quince + " zoop"); // 8
+	    } else {
+	        System.out.println("ik");    // 2
+	        baffle(quince);              // 3
+	        System.out.println("boo-wa-ha-ha"); // 9
+	    }
+	}
 }
